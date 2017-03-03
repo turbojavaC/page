9ea1179f5c873e6cfd7e50aac50f0d20f63a65cc
Adaweh Adawe <Adaweh Adawe>
2017-03-03 11:35:02 +0100

    modification du titre de la page

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+<title>Site web de Sam</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="form-group">
+			<nav class="nav navbar-default" style="background:#8e44ad;">
+				<div class="col-md-3" style="background:#2ecc71;">
+				<label for="search">Rechercher</label>
+				<input type="text" class="form-control" id="search" name="search" style="margin-top:10px;">
+				<button class="btn btn-success" style="margin-top:10px;">Boutton</button>
+				</div>
+				<div class="col-md-3" style="background:blue;">
+					<a href="home.html" style="text-decoration:none;color:cyan; font-size:45px; text-align:center; margin-top:15px;"><i class="fa fa-home">Home</i></a>
+				</div>
+				<div class="col-md-3" style="background:red;">
+				<a href="#" style="text-decoration:none;color:inherit; font-size:25px;"><i class="fa fa-video-camera">Video</i></a></div>
+				<div class="col-md-3" style="background:green;">
+					<a href="musics.html" style="text-decoration:none;color:inherit; font-size:25px;"><i class="fa fa-home">Musics</i></a>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="form-group">
+				<form method="" action="">
+					<label for="username">Username</label>
+					<input type="text" name="username" id="username" class="form-control">
+					<label for="email">Email</label>
+					<input type="email" name="email" id="email" class="form-control">	
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" class="form-control">
+					<button type="submit" class="btn btn-block btn-primary" style="margin-top:20px;"><i class="fa fa-sign-in">   Se connecter</i></button>
+				</form>
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
