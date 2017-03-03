325a693469c036cfdac75dc3edb13acb81137125
turbojavaC <turbojavapro@gmail.com>
2017-03-03 09:52:36 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Menu</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+<ul class="nav nav-pills" role="tablist">
+  <li role="presentation" class="active"><a href="#">Adaweh<span class="badge">42</span></a></li>
+  <li role="presentation"><a href="#">Connect</a></li>
+  <li role="presentation"><a href="#">Messages <span class="badge">6</span></a></li>
+</ul>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+<title>Page de Sam</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<h1>La page d'accueil</h1>
+</body>
+</html>
+
